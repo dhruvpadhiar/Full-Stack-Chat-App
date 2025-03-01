aa33812b94eb95e2dcb8bb7b901caeac05bba18f
Dhruv <dhruvpadhiar521@gmail.com>
2025-03-01 21:43:26 +0530

    auth signup

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,15 +1,16 @@
-import User from '../models/user.model.js'
+import { generateToken } from '../lib/utils.js';
+import {User} from '../models/user.model.js'
 import bcrypt from 'bcryptjs'
 
 export const signup = async (req, res) => {
-    const {fullName, email,  password}= req.body;
+    const {fullName, email,  password} = req.body;
     try {
         
         if(password.length < 6){
-            res.status(400).json({message: "Invalid Credentials"})  
+            res.status(400).json({message: "Password should be longer than 6 charac"})  
         }
 
-        const user = await User.findOne(email);
+        const user = await User.findOne({email});
         if(user) res.status(400).json({message: "User with the email already exists"})
         
         //hash passwords
@@ -23,12 +24,15 @@ export const signup = async (req, res) => {
         })
         
         if(newUser){
-
+            generateToken(newUser._id,res)
+            await newUser.save(); 
+            res.status(201).json({id: newUser._id, fullName:newUser.fullName, message: "User Created Successfully"})
         }else{
             res.status(400).json({message: "Invalid User Data"})
         }
 
     } catch (error) {
+        console.log(`Error while creating user (signup) ${error.message}`)
         res.status(500).json({message: "Something went wrong"})
     }
 }
