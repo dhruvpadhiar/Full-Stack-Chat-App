cb65ea4c7002ec37f373ec2183f6486162db95d7
Dhruv <dhruvpadhiar521@gmail.com>
2025-03-01 20:40:45 +0530

    backend setup

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,10 @@
+export const signup = async (req, res) => {
+
+}
+
+export const login = async (req, res) => {
+    
+}
+export const logout = async (req, res) => {
+    
+}
---
+++ backend/src/index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import authRoutes from './routes/auth.route.js';
+
+dotenv.config();
+
+const app = express();
+app.use(express.json())
+
+app.use('/api/auth', authRoutes);
+
+
+app.listen(process.env.PORT || 5001, () => {
+  console.log(`Server is running on http://localhost:${process.env.PORT}`);
+});
