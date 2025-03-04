92fab70223507738f797b0acd795b624180dd0ec
Dhruv <dhruvpadhiar521@gmail.com>
2025-03-05 00:47:09 +0530

    deployment

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,9 +7,19 @@ import cookieParser from 'cookie-parser';
 import cors from 'cors';
 
 import {app, server} from "./lib/socket.js"
+import path from "path";
+
 
 dotenv.config();
+const __dirname = path.resolve();
 
+if(process.env.NODE_ENV==="production"){
+  app.use(express.static(path.join(__dirname, "../frontend/dist")))
+
+  app.get("*", (req,res)=>{
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  })
+}
 
 
 app.use(express.json())
